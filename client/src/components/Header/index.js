8a1d103307a63c2d4431da92da62f82f07b969ec
BrandonKutivan <bskutivan@gmail.com>
2020-08-13 11:31:26 -0700

    converted sass files to css

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './styles.css';
+import Logo from './assets/images/templogo.jpg';
 
 const Header = props => {
     return (
         <header className="header">
-
+            <div className="wrap">
+                <div className="logo">
+                    <img src={Logo} alt="templogo"/>
+                </div>
+            </div>
         </header>
     );
-}
+};
+
+export default Header;
