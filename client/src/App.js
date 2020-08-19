73c52184bf44b670703096af2b0ddeaa21fb559d
BrandonKutivan <bskutivan@gmail.com>
2020-08-18 17:01:03 -0700

    reset app.js file again and incorporated newest front end code

--- client/src/App.js
+++ client/src/App.js
@@ -6,32 +6,62 @@ import HomepageLayout from './layouts/HomepageLayout';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
 import MainLayout from './layouts/MainLayout';
-import { Switch, Route, Redirect } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+const client = new ApolloClient({
+    request: (operation) => {
+      const token = localStorage.getItem('id_token')
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : ''
+        }
+      })
+    },
+    uri: '/graphql',
+});
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <div className="main">
-             <Switch>
-            <Route exact path="/" render={() => (
-                <HomepageLayout>
-                    <Homepage />
-                </HomepageLayout>
-            )} />
-            <Route path="/registration" render={() => currentUser ? <Redirect to="/" /> : (
-                <MainLayout currentUser={currentUser}>
-                    <Registration />
-                </MainLayout>
-            )} />
-            <Route path="/login" render={() => (
-                <MainLayout>
-                    <Login />
-                </MainLayout>
-            )} />
-             </Switch>
-            </div>
-        </div>
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <Provider store={store}>
+                    <div className="App">
+                        <Header />
+                        <div className="main">
+                        <Switch>
+                            <Route exact path="/" render={() => (
+                                <HomepageLayout>
+                                    <Homepage />
+                                </HomepageLayout>
+                            )} />
+                            <Route path="/registration" render={() => currentUser ? <Redirect to="/" /> : (
+                                <MainLayout currentUser={currentUser}>
+                                    <Registration />
+                                </MainLayout>
+                            )} />
+                            <Route path="/login" render={() => (
+                                <MainLayout>
+                                    <Login />
+                                </MainLayout>
+                            )} />
+                        </Switch>
+                        </div>
+                    </div>
+                    </Provider>
+                </div>
+            </Router>
+        </ApolloProvider>
+    )
+}
+function App() {
+    return (
+        
     );
 }
 
