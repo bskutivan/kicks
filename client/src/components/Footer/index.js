13c5166afadaa8d1cc9561ac2315e43052f7e40a
BrandonKutivan <bskutivan@gmail.com>
2020-08-20 12:36:35 -0700

    added twitter and insta icons

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import "./styles.css";
+import Insta from "../../assets/icons/instagram.png"
+import Twitter from "../../assets/icons/twitter.png"
 
 const Footer = props => {
     return (
         <footer className="footer">
             <div className="wrap">
                 <span>@2020 Kicks 👠</span> 
+            </div>
+            <div className="wrap">
+                <img alt="instagram" className="footer-img" src={Insta}  />
+                <img alt="twitter" className="footer-img" src={Twitter} />
             </div>
         </footer>
     );
