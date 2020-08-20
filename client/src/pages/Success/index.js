10e03b09d6fe82ef16e9de5eb1c697e3c2b92de9
BrandonKutivan <bskutivan@gmail.com>
2020-08-20 12:17:11 -0700

    added JSX return statement to success page

--- client/src/pages/Success/index.js
+++ client/src/pages/Success/index.js
@@ -7,6 +7,31 @@ function Success() {
     const [addOrder] = useMutation(ADD_ORDER);
 
     useEffect(() => {
-        
-    })
+        async function saveOrder() {
+            const cart = await idbPromise('cart', 'get');
+            const products = cart.map(item => item._id);
+
+            if (products.length) {
+                const { data } = await addOrder({ variables: { products } });
+                const productData = data.addOrder.products;
+
+                productData.forEach((item) => {
+                    idbPromise('cart', 'delete', item);
+                });
+            }
+
+            setTimeout(() => {
+                window.location.assign('http://localhost:3000/')
+            }, 3000);
+        }
+        saveOrder();
+    }, [addOrder]);
+
+    return (
+        <div>
+            <h1>New Shoes Incoming!</h1>
+            <h2>Thank you for placing your order!</h2>
+            <h2>This page will redirect you to the homepage but make sure to follow us below!</h2>
+        </div>
+    )
 }
