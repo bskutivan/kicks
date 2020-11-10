a84f13d32d356c8466e12be52ed78612d3c7235f
BrandonKutivan <bskutivan@gmail.com>
2020-11-09 18:02:58 -0800

    migrating to Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/kicks', {
+mongoose.connect(process.env.DB_URI || 'mongodb://localhost/kicks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
